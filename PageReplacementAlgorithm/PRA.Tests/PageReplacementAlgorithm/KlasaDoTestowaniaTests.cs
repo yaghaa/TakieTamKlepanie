73d5ef17673526d950a0861d37a2a1df208e07a6
yagha <za.rogiem@gmail.com>
2015-12-16 23:12:40 +0100

    test...

---
+++ PageReplacementAlgorithm/PRA.Tests/PageReplacementAlgorithm/KlasaDoTestowaniaTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using PageReplacementAlgorithm;
+using PageReplacementAlgorithm.Interfaces;
+
+namespace PRA.Tests.PageReplacementAlgorithm
+{   
+    [TestFixture]
+    class KlasaDoTestowaniaTests
+    {
+
+        private Mock<IPageReplacementAlgorithm> _algorithmnMock;
+        private int count = 0;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _algorithmnMock = new Mock<IPageReplacementAlgorithm>();
+            
+        }
+
+        [Test]
+        public void MetodaDoTestów_ReturnTrue_IfGoodValues()
+        {
+            //Arrange
+            var a = 0;
+            var b = 1;
+            count = 0;
+            _algorithmnMock.Setup(x=> x.Simulation(It.IsAny<List<int>>(),It.IsAny<int[]>())).Callback(()=> count++);
+            var testObject = new KlasaDoTestowania(_algorithmnMock.Object);
+            //Act
+
+            var result = testObject.MetodaDoTestów(a,b);
+
+            //ASSERT
+
+            Assert.AreEqual(result.Result, true);
+            Assert.IsInstanceOf<bool>(result.Result);
+            Assert.AreEqual(count, 1);
+
+        }
+
+
+        [Test]
+        public void MetodaDoTestów_AlgorithmThrowsException_IsCatched()
+        {
+            //Arrange
+            var a = 0;
+            var b = 1;
+            var list = new List<int>();
+            _algorithmnMock.Setup(x => x.Simulation(list, It.IsAny<int[]>())).Throws(new SuccessException("test"));
+            var testObject = new KlasaDoTestowania(_algorithmnMock.Object);
+            //Act
+
+            var result = testObject.MetodaDoTestów(a, b);
+
+            //ASSERT
+
+            Assert.AreEqual(result.HasException, true);
+            Assert.AreEqual(result.exception.Message,"testowy błąd");
+            Assert.IsInstanceOf<SuccessException>(result.HasException);
+
+        }
+    }
+}
---
+++ PageReplacementAlgorithm/PageReplacementAlgorithm/KlasaDoTestowania.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using PageReplacementAlgorithm.Interfaces;
+
+namespace PageReplacementAlgorithm
+{
+    public class KlasaDoTestowania
+    {
+
+        private IPageReplacementAlgorithm _algorithm;
+
+        public KlasaDoTestowania(IPageReplacementAlgorithm alg)
+        {
+            _algorithm = alg;
+        }
+
+        public OperationResult MetodaDoTestów(int a, int b)
+        {
+            try
+            {
+                _algorithm.Simulation(new List<int>(), new int[] { 1 });
+                if (a + b == 1)
+                {
+                    return new OperationResult(){Result = true,HasException = false};
+                }
+                return new OperationResult(){HasException = false,Result = false};
+            }
+            catch (Exception ex)
+            {
+                return new OperationResult() { HasException = true,exception = ex};
+                
+            }
+
+            
+            
+        }
+    }
+
+    public class OperationResult
+    {
+        public bool HasException;
+        public bool Result;
+        public Exception exception;
+
+
+
+    }
+}
